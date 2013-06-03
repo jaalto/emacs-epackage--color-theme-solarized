027535cdca4e1eaae517d4d7ce05c4f54c0ebd6b
Jari Aalto <jari.aalto@cante.net>
2013-06-03 11:31:43 +0300

    rm color-theme-solarized-epackage-xactivate.el (see *-examples.el instead)
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/color-theme-solarized-epackage-xactivate.el
+++
@@ -1,6 +0,0 @@
-(cond
- ((fboundp 'load-theme)			;Emacs 24
-  (load-theme 'solarized-light t))
- ((or (featurep 'color-theme)
-      (fboundp 'color-theme-install))
-  (require 'color-theme-solarized)))
