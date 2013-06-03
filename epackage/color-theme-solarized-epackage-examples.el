b1c9f531307fb043940b89fcbc3240598e341712
Jari Aalto <jari.aalto@cante.net>
2013-06-03 11:31:56 +0300

    color-theme-solarized-epackage-examples.el: add code example

--- epackage/color-theme-solarized-epackage-examples.el
+++ epackage/color-theme-solarized-epackage-examples.el
@@ -1,3 +1,16 @@
 ;; Prevent loading this file. Study the examples.
 (error "color-theme-solarized-epackage-examples.el is not a configuration file.")
+
+;; Code to copy to ~/.emacs
+;;
+;; Notice that in older Emacs, you need to install separate
+;; color-theme.el package
+
+(cond
+ ((fboundp 'load-theme)                 ;Emacs 24
+  (load-theme 'solarized-light t))	;See also *-dark theme
+ ((or (featurep 'color-theme)
+      (fboundp 'color-theme-install))
+  (require 'color-theme-solarized)))
+
 ;; End of file
